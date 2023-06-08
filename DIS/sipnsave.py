1b77b65eca1c01c0cf68242449dbdd4a9b49414b
cfp185 <cfp185@alumni.ku.dk>
2023-06-08 10:37:36 +0200

    blandet

--- DIS/sipnsave.py
+++ DIS/sipnsave.py
@@ -1,9 +1,28 @@
 from flask import Flask, render_template, url_for
 
+
 app = Flask(__name__)
 
 @app.route("/")
 def front_page():
     return render_template(
         "frontpage.html",
+    )
+
+@app.route("/drink")
+def drink_type():
+    return render_template(
+        "drinktype.html"
+    )
+
+@app.route("/result")
+def result():
+    return render_template(
+        "result.html"
+    )
+
+@app.route("/liar")
+def liar():
+    return render_template(
+        "liar.html"
     )
