dab97eec2dfbe59c4e698f92005838d0a873f946
KatjaBorchorst <wkl712@alumni.ku.dk>
2023-06-08 13:56:20 +0200

    rename app

--- DIS/app.py
+++ DIS/app.py
@@ -1,9 +1,35 @@
 from flask import Flask, render_template, url_for
 
+
 app = Flask(__name__)
+app.config
 
 @app.route("/")
 def front_page():
     return render_template(
         "frontpage.html",
+    )
+
+@app.route("/drink")
+def drink_type():
+    return render_template(
+        "drinktype.html"
+    )
+
+@app.route("/result")
+def result():
+    return render_template(
+        "result.html"
+    )
+
+@app.route("/areyousure")
+def are_you_sure():
+    return render_template(
+        "areyousure.html"
+    )
+
+@app.route("/mind")
+def mind():
+    return render_template(
+        "mind.html"
     )
